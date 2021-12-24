4e67c08c12fbf105e6c16367b6600d87ea5fdb0c
Vita Dovhan <87687287+Vita-Dovhan@users.noreply.github.com>
2021-12-24 06:08:18 +0200

    changed 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,26 +1,17 @@
-import Player from "@vimeo/player"
+import Player from "@vimeo/player";
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('timeupdate', throttle(videoStart, 1000))
-player.getCurrentTime().then((seconds) => {
-  localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
-});
+player.on('timeupdate', throttle(function () {
+  player.getCurrentTime().then(function (sec) {
+    console.log("time:", sec)
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(sec));
+  });
+}, 1000),
+)
 
 const timeStart = JSON.parse(localStorage.getItem("videoplayer-current-time")) || 0
-player.setCurrentTime(timeStart).then(function (timeStart) {
-}).catch(function (error) {
-  switch (error.name) {
-    case 'RangeError':
-      break;
-
-    default:
-
-      break;
-  }
-});
-function videoStart(timeStart) {
-  console.log(timeStart)
-}
+console.log(timeStart);
+player.setCurrentTime(timeStart)
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,7 +7,7 @@ const refs = {
 }
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(populateMessageOutput, 500));
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 1000));
 
 
 refs.form.addEventListener('input', (e) => {
@@ -22,17 +22,31 @@ function onFormSubmit(event) {
   event.preventDefault();
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY)
+
+
+
 }
-function populateMessageOutput() {
+
+function onTextareaInput(event) {
+  const message = event.target.value;
+}
+
+function populateVessageOutput() {
   const savedMessage = localStorage.getItem(STORAGE_KEY);
   if (savedMessage) {
-    refs.form.value = savedMessage;
+    refs.textarea.value = savedMessage;
   }
 }
 
 
 
 
+// console.log(localStorage)
+// localStorage.setItem('date', JSON.stringify({ name: 'vita', age: 33 }))
+// const savedData = localStorage.getItem('date')
+// console.log("saveddata:", savedData)
+// const parsedData = JSON.parse(savedData)
+// console.log("parseddata:", parsedData)
 
 
 
